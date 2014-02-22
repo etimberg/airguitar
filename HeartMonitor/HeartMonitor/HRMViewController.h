3887f6d83e822ed323e5c57a6fe1736f643b5502
Victor Zhang <stetorvs@gmail.com>
2014-02-22 17:31:07 -0500

    Minor.

--- HeartMonitor/HeartMonitor/HRMViewController.h
+++ HeartMonitor/HeartMonitor/HRMViewController.h
@@ -8,6 +8,18 @@
 
 #import <UIKit/UIKit.h>
 
+// Start tutorial code
+@import CoreBluetooth;
+@import QuartzCore;
+
+#define DEVICE_INFO_SERVICE_UUID @"180A"
+#define DEVICE_DATA_SERVICE_UUID @"180D"
+
+#define ACCEL_CHARACTERISTIC_UUID @"2A37"
+#define GYRO_CHARACTERISTIC_UUID @"2A38"
+#define MAGNETO_CHARACTERISTIC_UUID @"2A29"
+// End tutorial code
+
 @interface HRMViewController : UIViewController
 
 @end
