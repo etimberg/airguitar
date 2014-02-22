9afa430acd965ef5f0afe711e50f2eaf9dd43e64
etimberg <evert.timberg@gmail.com>
2014-02-22 12:48:23 -0500

    Initial Commit

---
+++ AirGuitar/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AirGuitar
+//
+//  Created by Evert Timberg on 2/22/2014.
+//  Copyright (c) 2014 Evert Timberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
